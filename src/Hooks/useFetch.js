16fef22abc1e200867f10c27f0a2ba3c153cc9de
sukhendra523 <523sukh@gmail.com>
2024-07-19 21:46:21 +0530

    Empty Commit

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -1,35 +1,33 @@
 import { useState } from "react";
 
+const useFetch = async (url, options = { method: "GET" }) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-const useFetch = async (url , options = {method:'GET'}) => {
+  try {
+    setLoading(true);
+    const response = await fetch(url, options);
 
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-
-    try {
-        setLoading(true);
-        const response = await fetch(url,options);
-
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        
-        const responseData  = await response.json();
-    
-        if(responseData){
-            setData(responseData);
-        }
-    } catch (error) {
-        console.error(error);
-        setError(error.body.message || error.message || `Sorry! Something went wrong`);
-        
-    } finally {
-        setLoading(false);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
 
-    return { data , loading , error };
-}
+    const responseData = await response.json();
 
+    if (responseData) {
+      setData(responseData);
+    }
+  } catch (error) {
+    console.error(error);
+    setError(
+      error.body.message || error.message || `Sorry! Something went wrong`
+    );
+  } finally {
+    setLoading(false);
+  }
+
+  return { data, loading, error };
+};
 
 export default useFetch;
